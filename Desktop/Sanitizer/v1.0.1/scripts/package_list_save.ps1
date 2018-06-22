a3361bb5ffd831b2fb31b00fe637664be3ba2fa0
Aaron Sigal <asigal55@gmail.com>
2018-06-22 09:07:30 -0700

    Started versioning.

---
+++ Desktop/Sanitizer/v1.0.1/scripts/package_list_save.ps1
@@ -0,0 +1,7 @@
+New-Item -ItemType Directory -Force -Path C:\logs
+
+Start-Transcript -Path ts.txt -Force
+
+Get-AppxPackage
+
+Stop-transcript
---
+++ Desktop/Sanitizer/v1.0.1/src/Make.bat
@@ -0,0 +1,1 @@
+g++ -std=c++11 -static-libgcc -static-libstdc++ Sanitizer.cpp
